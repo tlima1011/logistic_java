26faebf0452a25c104139ea6638dbe437a6b4537
tlima1011 <thiagolima1011@gmail.com>
2026-04-02 22:01:03 -0300

    Classe Cliente Pessoa Juridica

---
+++ src/com/logistic_java/entities/ClienteJuridica.java
@@ -0,0 +1,13 @@
+package com.logistic_java.entities;
+
+public class ClienteJuridica extends Cliente{
+	
+	private String ie; 
+	private String cnpj; 
+	
+	public ClienteJuridica(String n) {
+		super(n);
+		// TODO Auto-generated constructor stub
+	}
+	
+}
